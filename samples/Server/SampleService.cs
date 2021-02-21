7ae536a9598a52a42489402a9709700d955effd5
Rachel <guotianpei@gmail.com>
2021-02-21 15:35:46 -0500

    Initial checkin

---
+++ samples/Server/SampleService.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using MHPService;
+using Server.Interfaces;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using SoapCore.Extensibility;
+
+namespace Server
+{
+	public class SampleService : ISampleService
+	{
+		 
+		private MHPServicesClient _mhpClient;
+		//private VUEServicesClient _vueClient;
+
+		public void SampleSerice()
+        {
+			
+			IMessageInspector messageInspector = new SoapSecurityHeaderInspector();
+
+			_mhpClient = new MHPServicesClient();
+			var requestInterceptor = new FilteringEndpointBehavior(messageInspector);
+			_mhpClient.Endpoint.EndpointBehaviors.Add(requestInterceptor);
+
+		}
+
+		public Task<GetCityStateByZipCodeResponse> GetCityStateByZipCodeRequest(string zipCode, string envId)
+		{
+			Task<GetCityStateByZipCodeResponse> response = _mhpClient.GetCityStateByZipCodeAsync(zipCode, envId);
+			return response;
+		}
+
+		 
+
+    }
+}
---
+++ samples/Server/WsAuthToken.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace Server
+{
+    
+	[XmlRoot("AuthToken", Namespace = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd")]
+	public class WsAuthToken
+	{
+		[XmlElement("REQUESTORSYSTEM")]
+		public string Requestor { get; set; }
+
+		[XmlElement("SUBSCRIBERSYSTEM")]
+		public string Subscriber { get; set; }
+
+
+		[XmlElement("REQUESTTIMESTAMP")]
+		public XmlDateTimeSerializationMode RequestTime { get; set; }
+
+		
+	}
+}
